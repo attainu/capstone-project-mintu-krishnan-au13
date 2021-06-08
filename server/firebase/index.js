43c76a1412cfd287eb854eba3b9ea586b7299099
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-06-08 16:26:22 +0530

    updated

---
+++ server/firebase/index.js
@@ -0,0 +1,8 @@
+var admin = require('firebase-admin');
+
+var serviceAccount = require('../config/fbServiceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://ecommerce-66e87-default-rtdb.firebaseio.com',
+});
