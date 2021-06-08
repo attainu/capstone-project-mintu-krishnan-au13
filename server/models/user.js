28912325c1febd3648b23c350ea259b1447cf239
dimple-shah-au13 <dimps287980@gmail.com>
2021-06-08 15:44:14 +0530

    updated default

--- server/models/user.js
+++ server/models/user.js
@@ -1,36 +1,37 @@
 const mongoose = require("mongoose");
-const {
-  ObjectId
-} = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "User must have a name!"],
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User must have a name!"],
+    },
+    email: {
+      type: String,
+      required: [true, "User must have an email!"],
+      index: true,
+    },
+    role: {
+      type: String,
+      default: "subscriber",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: {
+      type: String,
+    },
+    wishlist: {
+      type: ObjectId,
+      ref: "Product",
+      required: [true, "Wishlist must belong to a Product!"],
+    },
   },
-  email: {
-    type: String,
-    required: [true, "User must have an email!"],
-    index: true,
-  },
-  role: {
-    type: String,
-    default: "subscriber",
-  },
-  cart: {
-    type: Array,
-    default: [],
-  },
-  address: {
-    type: String,
-  },
-  wishlist: {
-    type: ObjectId,
-    ref: "Product",
-    required: [true, "Wishlist must belong to a Product!"],
-  },
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
