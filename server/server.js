eb9de69c4bc67c480e749e72690e5cda8f10f487
dimple-shah-au13 <dimps287980@gmail.com>
2021-06-08 12:55:14 +0530

    basic backend setup installed

--- server/server.js
+++ server/server.js
@@ -1,1 +1,41 @@
-console.log("Hiii")
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+require('dotenv').config();
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+mongoose
+  .connect(process.env.L_DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() =>
+    console.log(chalk.black.bgGreen(' 🟢 DB connection successful! '))
+  )
+  .catch((err) => console.log(chalk.redBright(err)));
+
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cors());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.get('/api', (req, res) => {
+  res.json({ data: 'working' });
+});
+
+app.listen(port, () => {
+  console.log(chalk.bgYellow.black(` App running on port ${port}... `));
+});
