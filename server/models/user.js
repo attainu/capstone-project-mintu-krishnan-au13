26f7b44ff3bf11e61c9ddc89fbaeee586f175f44
dimple-shah-au13 <dimps287980@gmail.com>
2021-06-08 15:00:43 +0530

    created model schema

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'User must have a name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'User must have an email!'],
+    index: true,
+  },
+  role: {
+    type: String,
+    default: 'subscriber',
+  },
+  cart: {
+    type: Array,
+    default: [],
+  },
+  address: {
+    type: String,
+  },
+  //   wishlist: {
+  //     type: ObjectId,
+  //     ref: 'Product',
+  //     required: [true, 'Wishlist must belong to a Product!'],
+  //   },
+});
+
+module.exports = mongoose.model('User', userSchema);
