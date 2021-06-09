68674be00cf8fa7b250bae18a972ca7ce993c7f8
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-06-09 22:38:35 +0530

    removed clg

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -12,9 +12,7 @@ exports.createOrUpdateUser = async (req, res) => {
     { name, picture },
     { new: true }
   );
-
   if (user) {
-    console.log('updated user');
     res.json(user);
   } else {
     const newUser = await new User({
@@ -22,13 +20,11 @@ exports.createOrUpdateUser = async (req, res) => {
       name,
       picture,
     }).save();
-    console.log('new user');
     res.json(newUser);
   }
 };
 
 exports.currentUser = async (req, res) => {
-  console.log('hhhhh');
   await User.findOne({ email: req.user.email }).exec((err, user) => {
     if (err) throw new Error(err);
     res.json(user);
