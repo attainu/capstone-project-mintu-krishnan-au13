0b3a5f7d6c6e480f40b9610873236da6fe1b9673
dimple-shah-au13 <dimps287980@gmail.com>
2021-06-09 17:53:50 +0530

    created adminCheck middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,5 +9,6 @@ const { createOrUpdateUser, currentUser } = require('../controllers/auth');
 
 router.post('/create-or-update-user', authCheck, createOrUpdateUser);
 router.post('/current-user', authCheck, currentUser);
+router.post('/current-admin', authCheck, adminCheck, currentUser);
 
 module.exports = router;
