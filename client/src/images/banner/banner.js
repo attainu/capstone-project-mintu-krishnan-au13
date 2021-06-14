d08b8d508e26650ec52e28c2af26042cd707d916
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-06-14 20:12:14 +0530

    updated images

---
+++ client/src/images/banner/banner.js
@@ -0,0 +1,28 @@
+const IMAGES = [
+  {
+    id: '001',
+    image: require('../banner/banner1.jpeg').default,
+  },
+  {
+    id: '002',
+    image: require('../banner/banner2.jpeg').default,
+  },
+  {
+    id: '003',
+    image: require('../banner/banner3.jpeg').default,
+  },
+  {
+    id: '004',
+    image: require('../banner/banner4.jpeg').default,
+  },
+  {
+    id: '005',
+    image: require('../banner/banner5.jpeg').default,
+  },
+  {
+    id: '006',
+    image: require('../banner/banner6.jpeg').default,
+  },
+];
+
+export default IMAGES;
---
+++ client/src/images/slides/slides.js
@@ -0,0 +1,49 @@
+const SLIDES = [
+  {
+    id: '001',
+    category: 'Kitchen Appliances',
+    image: require('../slides/slide1.svg').default,
+  },
+  {
+    id: '002',
+    category: 'Zip Services',
+    image: require('../slides/slide2.svg').default,
+  },
+  {
+    id: '003',
+    category: 'Consumables',
+    image: require('../slides/slide3.svg').default,
+  },
+  {
+    id: '004',
+    category: 'Cameras',
+    image: require('../slides/slide4.svg').default,
+  },
+  {
+    id: '005',
+    category: 'Top Brands',
+    image: require('../slides/slide5.svg').default,
+  },
+  {
+    id: '006',
+    category: 'Gaming',
+    image: require('../slides/slide6.svg').default,
+  },
+  {
+    id: '007',
+    category: 'Cameras',
+    image: require('../slides/slide4.svg').default,
+  },
+  {
+    id: '008',
+    category: 'Top Brands',
+    image: require('../slides/slide5.svg').default,
+  },
+  {
+    id: '009',
+    category: 'Gaming',
+    image: require('../slides/slide6.svg').default,
+  },
+];
+
+export default SLIDES;
